31a0d34a385214f54110effc296c221ced855fff
ravi <ravi.sharma2695@gmail.com>
2019-03-23 20:46:18 +0530

    Initial commit

---
+++ app/src/main/java/com/example/ravi/species/login_page.java
@@ -0,0 +1,14 @@
+package com.example.ravi.species;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class login_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_page);
+
+    }
+}
